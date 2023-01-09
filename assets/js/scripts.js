1b69b624a3ad74435f2a43c1d814e0cf5d84d652
Jamal KHALIS <39938150+jamalkhalis@users.noreply.github.com>
2023-01-09 22:46:46 +0100

    update

--- assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-var tabEl = document.querySelector('button[data-bs-toggle="tab"]')
-tabEl.addEventListener('hide.bs.tab', function (event) {
-
-  event.target.style.border = "0px"; // newly activated tab
-  event.relatedTarget.style.border = "0px"; // previous active tab
-
-})
